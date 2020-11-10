6f004f8f5bf0b5a21225b367ad815e493f7ffac8
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2020-11-10 18:29:26 +0100

    Module 6.2 - First Test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int addCal(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractCal(int a, int b) {
+        return a - b;
+    }
+}
