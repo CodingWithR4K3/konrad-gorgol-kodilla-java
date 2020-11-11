7dffe0a9f3936da6ec7f606243e50f41f0d2cbe6
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2020-11-11 14:05:55 +0100

    Module 6.3 - OddNumbersExterminator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,26 +1,9 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-
-        Calculator calculator = new Calculator();
-
-        int addResult = 20;
-        int subResult = 50;
-
-        if (addResult == calculator.addCal(15,5)) {
-            System.out.println("Addition works!");
-        } else {
-            System.out.println("Addition failed!");
-        }
-
-        if (subResult == calculator.subtractCal(100,50)) {
-            System.out.println("Subtraction works!");
-        } else {
-            System.out.println("Subtraction failed!");
-        }
-
     }
 }
