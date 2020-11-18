59779080fa8b7891616b5af4fad0a9e88fd5fcf5
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2020-11-18 15:32:13 +0100

    Module 7.4 -- World

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Continent {
+    private final List<Country> countryList = new ArrayList<>();
+
+    public void addCountry(final Country countryToAdd) {
+        countryList.add(countryToAdd);
+    }
+
+    public List<Country> getCountryList() {
+        return countryList;
+    }
+}
