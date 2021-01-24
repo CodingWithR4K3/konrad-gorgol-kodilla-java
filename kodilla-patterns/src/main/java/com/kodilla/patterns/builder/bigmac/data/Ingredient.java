528a2b4c1cb6f353f7906940b55c958119ca67a0
R4K3 <73075897+CodingWithR4K3@users.noreply.github.com>
2021-01-24 16:45:29 +0100

    Module 15.5 - Task

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/data/Ingredient.java
@@ -0,0 +1,15 @@
+package com.kodilla.patterns.builder.bigmac.data;
+
+public enum Ingredient {
+    LETTUCE("Lettuce"),
+    ONION("Onion"),
+    BACON("Bacon"),
+    CUCUMBER("Cucumber"),
+    CHILI_PEPPER("Chili Pepper"),
+    MUSHROOMS("Mushrooms"),
+    SHRIMPS("Shrimps"),
+    CHEESE("Cheese");
+
+    Ingredient(String ingredientName) {
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/data/Sauce.java
@@ -0,0 +1,10 @@
+package com.kodilla.patterns.builder.bigmac.data;
+
+public enum Sauce {
+    STANDARD("Standard"),
+    THOUSAND_ISLANDS("1000 Islands"),
+    BARBECUE("Barbecue");
+
+    Sauce(String sauceType) {
+    }
+}
