7ed52fafca17138e9c5f964589d22b352ebe4824
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2021-02-17 14:24:06 +0100

    Module 17.2 - Task

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,56 @@
+package com.kodilla.hibernate.tasklist;
+
+import com.kodilla.hibernate.task.Task;
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TASKLISTS")
+public class TaskList {
+    private final List<Task> tasks = new ArrayList<>();
+    private int id;
+    private String listName;
+    private String description;
+
+    public TaskList() {
+    }
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "ID")
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "LISTNAME", unique = true)
+    public String getListName() {
+        return listName;
+    }
+
+    private void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+}
+
